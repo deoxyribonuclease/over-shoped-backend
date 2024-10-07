3a004a4aad8587a72b3640a5bd7ff2dd870f3c7e
Quo-len <kotenko.vadym1121@vu.cdu.edu.ua>
2024-10-07 21:10:54 +0300

    Initial commit

---
+++ src/test/java/com/example/overshopedbackend/OverShopedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.overshopedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverShopedBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
